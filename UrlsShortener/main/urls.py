11e06e810f2219fca60390fe4a3a3a5ee5c549ac
krivosheeverik <40608785+krivosheeverik@users.noreply.github.com>
2019-11-06 21:34:14 +0300

    v0.0.2

--- UrlsShortener/main/urls.py
+++ UrlsShortener/main/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-]
+from main.views import (main_view,
+                        )
+
+urlpatterns= [
+    path('',main_view, name='main'),
+    ]
